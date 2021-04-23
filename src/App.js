d484e06d806b0736923cbb73efda93ca37b7b428
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2021-04-24 01:12:35 +0600

    react-redux-01 done spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
 import "./styles.css";
+import { connect } from "react-redux";
+import { startAction } from "./actions/startAction";
+import { stopAction } from "./actions/stopAction";
+import rotateAction from "./actions/rotateAction";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <img
+        src={`https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png`}
+        className={"App-logo" + (this.props.rotating ? "" : " App-logo-paused")}
+        alt="logo"
+        onClick={() => {
+          this.props.rotateAction(!this.props.rotating);
+        }}
+      />
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  ...state
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction),
+  rotateAction: (payload) => dispatch(rotateAction(payload))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./App";
+import rotateReducer from "./reducers/rotateReducer";
+
+const store = createStore(rotateReducer, (state = { rotating: true }));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   rootElement
 );
